77887b0f647d92803dc99a484a60a70cdda232a1
SugarAdam <631364481@qq.com>
2016-05-16 22:07:51 +0800

    继续写一堆cell 写完了已经 刷新cell列表的代理还没写完。。。

--- QuFaXian/QuFaXian/Cell/发现/CreaterContentHeader.h
+++ QuFaXian/QuFaXian/Cell/发现/CreaterContentHeader.h
@@ -7,13 +7,41 @@
 //
 
 #import <UIKit/UIKit.h>
+/**
+ *  点击内容 讨论时 切换详情VC的下面的cell列表
+ */
+@protocol ClickBtnToRefreshListDelegate <NSObject>
+
+- (void)clickBtnToRefreshList;
+
+@end
 
 @interface CreaterContentHeader : UITableViewHeaderFooterView
 
+@property (nonatomic, assign) id<ClickBtnToRefreshListDelegate> delegate;
+
 @property (weak, nonatomic) IBOutlet UIImageView *headerBackImg;
+
+@property (weak, nonatomic) IBOutlet UIImageView *headerBigHeadImage;
+
+@property (weak, nonatomic) IBOutlet UILabel *stateName;
 
 @property (weak, nonatomic) IBOutlet UIButton *discussBtn;
 
+@property (weak, nonatomic) IBOutlet UIButton *contentsBtn;
+
 @property (weak, nonatomic) IBOutlet UILabel *focusNum;
+
+@property (weak, nonatomic) IBOutlet UILabel *stateDescription;
+
+@property (weak, nonatomic) IBOutlet UIImageView *stateCreateHeadImg;
+
+@property (weak, nonatomic) IBOutlet UILabel *stateCreateName;
+
+@property (weak, nonatomic) IBOutlet UILabel *stateCreateIntroduction;
+
+//header类型为内容站的时候显示该view
+@property (weak, nonatomic) IBOutlet UIView *contentsStateView;
+
 
 @end
